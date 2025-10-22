feb3470399b32275c07a297f3d368d71828227c4
dqublebubble <nice.doter123@mail.ru>
2025-10-22 22:30:45 +0300

    smoke тест /pet/findByStatus с двумя параметрами и проверкой на 200 statusCode

--- src/test/java/controller/PetControllerSmokeTests.java
+++ src/test/java/controller/PetControllerSmokeTests.java
@@ -10,7 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 @ContextConfiguration(classes = TestsConfigurations.class)
@@ -30,5 +37,28 @@ public class PetControllerSmokeTests {
         HttpStatusCode actutalStatusCode = forEntity.getStatusCode();
 
         Assertions.assertEquals(expectedStatusCode, actutalStatusCode);
+    }
+
+    @Test
+    @DisplayName("SMOKE: GET /pet/findByStatus?status={status}&status={status} - корректный код возврата" +
+            " при валидном запросе с двумя параметреми")
+    void getRequestPetControllerFindByStatus() {
+        HttpStatusCode expectedStatusCode = HttpStatusCode.valueOf(200);
+
+        URI uri = UriComponentsBuilder
+                .fromUriString(BASE_URL + "/pet/findByStatus")
+                .queryParam("status", "pending", "sold")
+                .build()
+                //выше все собрали в UriComponents - хост, путь и тд
+                .encode()
+                //через encode безопасное кодирование (Name Surname -> Name%20Surname)
+                .toUri();
+
+        ResponseEntity<String> forEntity= restTemplate.getForEntity(uri, String.class);
+        HttpStatusCode actualStatusCode = forEntity.getStatusCode();
+
+        Assertions.assertEquals(expectedStatusCode, actualStatusCode);
+
+
     }
 }
